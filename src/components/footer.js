3c9d6bed2c5a73f07731f9127abdb7d40ac9d9cf
Afshin <99084497+AfshinTk@users.noreply.github.com>
2023-06-06 19:15:30 +0330

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,8 +8,8 @@ export default class Footer extends Component {
                 <div className="container">
                   <div className="row">
                     <div className="col l6 s12">
-                      <h5 className="white-text">Footer Content</h5>
-                      <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                      <h5 className="white-text">Sensors Monitoring</h5>
+                      <p className="grey-text text-lighten-4">measured values will showed here.</p>
                     </div>
                     <div className="col l4 offset-l2 s12">
                       <h5 className="white-text">Links</h5>
